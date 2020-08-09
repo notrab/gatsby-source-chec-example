856f78c529aa29dd28eb440f8e70a1aec1943734
Jamie Barton <jamie@notrab.dev>
2020-08-09 14:46:48 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "@chec/gatsby-source-chec",
+      options: {
+        publicKey: process.env.CHEC_PUBLIC_KEY,
+      },
+    },
+  ],
+};
